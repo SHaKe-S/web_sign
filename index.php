caa5c54d81ee911d52f66c9e0648f7a864a795e2
Your Name <you@example.com>
2025-01-31 15:17:45 +0400

    переработал структуру теперь index является хабом где авторизованный пользователь переходит на страницу обратной свзяи, если нет то отправляется на регистрацию

--- index.php
+++ index.php
@@ -1,25 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-    <form action="register.php" method="post">
-            <input type="text"  placeholder="login" name="login">
-            <input type="text"  placeholder="password" name="pass">           
-            <input type="text"  placeholder="repeat password" name="repeatpass">
-            <input type="text"  placeholder="email" name="email">
-            <button type="submit">Зарегистрироваться</button>
-    </form>
-
-    <form action="login.php" method="post">
-            <input type="text"  placeholder="login" name="login">
-            <input type="text"  placeholder="password" name="pass"> 
-            <button type="submit">Войти</button>          
-    </form>
-
-</body>
-</html>
+<?php
+session_start();
+if (isset($_SESSION['user'])) {
+    header("Location: feedback/feedback.php");
+    exit();
+} else {
+    header("Location: auth/login.php");
+    exit();
+}
+?>
